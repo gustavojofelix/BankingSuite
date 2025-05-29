fb3b60cd7ab7db9bdeb5958473060516b5fba746
Gustavo Felix <gustavo.felix@alvorsoftware.com>
2025-05-29 13:19:38 +0200

    Account Service implementation

--- AccountService/Program.cs
+++ AccountService/Program.cs
@@ -1,10 +1,20 @@
+using AccountService.Data;
+using AccountService.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AccountDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<AccountRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -12,6 +22,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+  app.UseSwagger();
+app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
