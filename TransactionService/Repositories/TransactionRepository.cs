e7c08f05023950e4da4feef33bbcb5d00b168d25
Gustavo Felix <gustavo.felix@alvorsoftware.com>
2025-05-29 18:50:31 +0200

    feat: Transaction Service

---
+++ TransactionService/Repositories/TransactionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TransactionService.Data;
+using TransactionService.Models;
+
+namespace TransactionService.Repositories
+{
+  public class TransactionRepository
+  {
+    private readonly TransactionDbContext _context;
+    public TransactionRepository(TransactionDbContext context) => _context = context;
+
+    public async Task<List<Transaction>> GetAllAsync() => await _context.Transactions.ToListAsync();
+
+    public async Task<Transaction> CreateAsync(Transaction txn)
+    {
+      _context.Transactions.Add(txn);
+      await _context.SaveChangesAsync();
+      return txn;
+    }
+  }
+}
